8a77d03115e4e49f853fcdca3ad315e5118932da
endlesssjourney2 <endlessj297@gmail.com>
2026-02-24 17:59:51 +0100

    Added drawer to navbar instead of links to pages(not last version)

---
+++ src/components/CustomDrawer/CustomDrawer.tsx
@@ -0,0 +1,39 @@
+import s from "./CustomDrawer.module.css";
+import type { FC } from "react";
+import { Divider, Drawer } from "@mui/material";
+
+type CustomDrawerProps = {
+  onClose: () => void;
+  open: boolean;
+  content: React.ReactNode;
+};
+
+const CustomDrawer: FC<CustomDrawerProps> = ({ onClose, open, content }) => {
+  const DrawerList = (
+    <div className={s.drawer} onClick={onClose}>
+      <div className={s.drawerHeader}>
+        <h2>Choose a page</h2>
+      </div>
+      <Divider sx={{ background: "#ffffffa1" }} />
+      <div className={s.drawerContent}>{content}</div>
+    </div>
+  );
+
+  return (
+    <Drawer
+      onClose={onClose}
+      open={open}
+      anchor="right"
+      sx={{
+        "& .MuiDrawer-paper": {
+          backgroundColor: "#1e1e1e",
+          color: "#fff",
+        },
+      }}
+    >
+      {DrawerList}
+    </Drawer>
+  );
+};
+
+export default CustomDrawer;

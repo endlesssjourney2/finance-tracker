996f4d66205f05438704cdc7b9a5d2b97ebaf9ef
endlesssjourney2 <endlessj297@gmail.com>
2026-04-14 19:18:59 +0200

    update UI on the Import page in light of last changes. add a seperate component for the guide

--- src/pages/Import/Import.tsx
+++ src/pages/Import/Import.tsx
@@ -1,7 +1,7 @@
-import { Divider } from "@mui/material";
 import Header from "../../components/Header/Header";
 import { useExpenses } from "../../hooks/useExpenses";
 import s from "./Import.module.css";
+import Guide from "./components/Guide";
 
 const Import = () => {
   const { importExpensesCsv, importExpensesJSON } = useExpenses();
@@ -56,64 +56,12 @@ const Import = () => {
         </div>
         <div className={s.description}>
           <h3 className={s.descHeading}>
-            Please note that you can only import files in the same format in
-            which you exported them.
+            Please note that you can only import files in the same format as in
+            guides below. If your file structure is different, the import will
+            fail.
           </h3>
         </div>
-        <div className={s.guide}>
-          <div className={s.titleCont}>
-            <h2 className={s.guideTitle}>How does it work?</h2>
-          </div>
-          <div className={s.guideCont}>
-            <div className={s.card}>
-              <h2 className={s.cardTitle}>JSON</h2>
-              <Divider sx={{ background: "#000000", width: "100%" }} />
-              <p className={s.text}>
-                To import expenses using JSON, your file must include all
-                database fields except <b>id</b> and <b>user_id</b>. These
-                fields are generated automatically.
-              </p>
-              <p className={s.text}>Example of a valid JSON structure:</p>
-
-              <pre className={s.code}>
-                {`{
-  "expenses": [
-    {
-      "amount": 100,
-      "category": "Food(category)",
-      "description": "lunch(description)",
-      "date": "2026-03-01T12:00:00+00:00"
-    }
-  ]
-}`}
-              </pre>
-              <p className={s.warning}>
-                Do not include <b>id</b> or <b>user_id</b> fields. They will be
-                added automatically.
-              </p>
-            </div>
-            <div className={s.card}>
-              <h2 className={s.cardTitle}>CSV</h2>
-              <Divider sx={{ background: "#000000", width: "100%" }} />
-              <p className={s.text}>
-                The CSV file must contain the same fields as the database except
-                <b> id</b> and <b>user_id</b>. These fields are generated
-                automatically.
-              </p>
-              <p className={s.text}>Example of a valid CSV structure:</p>
-
-              <pre className={s.code}>
-                {`amount,category,description,date
-100(amount),Food(category),lunch(description),2026-03-01T12:00:00+00:00`}
-              </pre>
-              <p className={s.warning}>
-                The first row <b>must</b> contain column names. Do not include{" "}
-                <b>id</b> or <b>user_id</b> fields. They will be added
-                automatically.
-              </p>
-            </div>
-          </div>
-        </div>
+        <Guide />
       </div>
     </div>
   );
